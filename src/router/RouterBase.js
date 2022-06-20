1d7c8d02bde8040edb08c9b97cae9d23817dfea3
CHRISTIAN JOSUE ROMERO LEON <cromero@tisoluciones.net.pe>
2022-06-19 20:07:00 -0500

    test router v2

--- src/router/RouterBase.js
+++ src/router/RouterBase.js
@@ -11,15 +11,14 @@ const RouterBase = () => {
     <HashRouter>
       <Routes>
         <Route path="/" element={<MenuApp />}/>
-        <Route path="repo" element={<MenuApp />}>
-          <Route path="app">
+        
+          <Route path="app" element={<MenuApp />}>
             <Route path="26-06-2022" element={<TwentySixPage />}/>
             <Route path="batman" element={<BatmanPage />}/>
             <Route path="about" element={<AboutPage />}/>
             <Route path="*" element={<div className="flex justify-center" ><p className="font-lobster text-4xl font-semibold  text-gray-700">Selecciona una opción del Menu</p></div>}/>
           </Route> 
-          <Route path="*" element={<div className="flex justify-center items-center" ><Link to="repo/app" className=" font-lobster text-5xl mr-5 font-semibold text-amber-800 no-underline  hover:text-black hover:underline">Ver Menú</Link></div>}/>       
-        </Route>
+        
         <Route path="*" element={<div className="flex justify-center items-center" ><Link to="repo/app" className=" font-lobster text-5xl mr-5 font-semibold text-amber-800 no-underline  hover:text-black hover:underline">Ver Menú</Link></div>}/>
       </Routes>
     </HashRouter>
