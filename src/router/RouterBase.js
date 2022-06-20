f6b9b7823565bc3aef24311c764cfd77b748b897
CHRISTIAN JOSUE ROMERO LEON <cromero@tisoluciones.net.pe>
2022-06-19 19:50:03 -0500

    change menu

--- src/router/RouterBase.js
+++ src/router/RouterBase.js
@@ -11,13 +11,16 @@ const RouterBase = () => {
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<MenuApp />}/>
-        <Route path="app" element={<MenuApp />}>
-          <Route path="26-06-2022" element={<TwentySixPage />}/>
-          <Route path="batman" element={<BatmanPage />}/>
-          <Route path="about" element={<AboutPage />}/>
-          <Route path="*" element={<div className="flex justify-center" ><p className="font-lobster text-4xl font-semibold  text-gray-700">Selecciona una opción del Menu</p></div>}/>
+        <Route path="repo" element={<MenuApp />}>
+          <Route path="app">
+            <Route path="26-06-2022" element={<TwentySixPage />}/>
+            <Route path="batman" element={<BatmanPage />}/>
+            <Route path="about" element={<AboutPage />}/>
+            <Route path="*" element={<div className="flex justify-center" ><p className="font-lobster text-4xl font-semibold  text-gray-700">Selecciona una opción del Menu</p></div>}/>
+          </Route> 
+          <Route path="*" element={<div className="flex justify-center items-center" ><Link to="repo/app" className=" font-lobster text-5xl mr-5 font-semibold text-amber-800 no-underline  hover:text-black hover:underline">Ver Menú</Link></div>}/>       
         </Route>
-        <Route path="*" element={<div className="flex justify-center items-center" ><Link to="app" className=" font-lobster text-5xl mr-5 font-semibold text-amber-800 no-underline  hover:text-black hover:underline">Ver Menú</Link></div>}/>
+        <Route path="*" element={<div className="flex justify-center items-center" ><Link to="repo/app" className=" font-lobster text-5xl mr-5 font-semibold text-amber-800 no-underline  hover:text-black hover:underline">Ver Menú</Link></div>}/>
       </Routes>
     </BrowserRouter>
   );
