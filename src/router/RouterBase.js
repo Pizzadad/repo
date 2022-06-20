82e65d5c98e279403c4081f90cd5697b82ec00d6
CHRISTIAN JOSUE ROMERO LEON <cromero@tisoluciones.net.pe>
2022-06-19 19:16:30 -0500

    first commit

--- src/router/RouterBase.js
+++ src/router/RouterBase.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
 import MenuApp from "../Components/MenuApp";
 import TwentySixPage from "../Components/TwentySixPage";
 import BatmanPage from "../Components/BatmanPage";
@@ -15,8 +15,9 @@ const RouterBase = () => {
           <Route path="26-06-2022" element={<TwentySixPage />}/>
           <Route path="batman" element={<BatmanPage />}/>
           <Route path="about" element={<AboutPage />}/>
-          <Route path="*" element={<MenuApp />}/>
+          <Route path="*" element={<div className="flex justify-center" ><p className="font-lobster text-4xl font-semibold  text-gray-700">Selecciona una opción del Menu</p></div>}/>
         </Route>
+        <Route path="*" element={<div className="flex justify-center items-center" ><Link to="app" className=" font-lobster text-5xl mr-5 font-semibold text-amber-800 no-underline  hover:text-black hover:underline">Ver Menú</Link></div>}/>
       </Routes>
     </BrowserRouter>
   );
